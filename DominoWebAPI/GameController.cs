21c803b5bba0fbd0ac227e3da10376750d2f6672
Farrel Emerson <FarrelEmerson02@gmail.com>
2026-08-19 14:31:40 +0700

    Moved Tiles into gamecontroller

--- DominoWebAPI/GameController.cs
+++ DominoWebAPI/GameController.cs
@@ -175,3 +175,15 @@ public class GameController
 
 
 }
+
+public readonly struct DominoTile
+{
+    public int Left { get; }
+    public int Right { get; }
+
+    public DominoTile(int left, int right)
+    {
+        Left = left;
+        Right = right;
+    }
+}
