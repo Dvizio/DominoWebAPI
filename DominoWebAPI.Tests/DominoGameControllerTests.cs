2f6c7a8aec24043e840405a1d12525c8bb95d782
Farrel Emerson <FarrelEmerson02@gmail.com>
2026-09-02 10:57:49 +0700

    init DominoControllerTests.cs

---
+++ DominoWebAPI.Tests/DominoGameControllerTests.cs
@@ -0,0 +1,20 @@
+namespace DominoWebAPI.Tests;
+
+using NUnit.Framework;
+using DominoWebAPI.Models;
+using DominoWebAPI.Controllers;
+using DominoWebAPI.DTOs;
+
+public class DominoGameControllerTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
